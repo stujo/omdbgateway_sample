8508e16a69b49c0a590b7353612446ee657ef9de
Stuart Jones <stujo@users.noreply.github.com>
2014-04-18 20:49:32 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,13 @@ require 'omdbgateway'
 
 p OMDBGateway.gateway.free_search('Jedi').body
 
+p OMDBGateway.gateway.find_by_id('tt0297410').body
+
+p OMDBGateway.gateway.find_by_id('tt0297CCC').success?
+
+p OMDBGateway.gateway.find_by_id('tt1322836').success?
+
+p OMDBGateway.gateway.find_by_id('tt1322836').prune_hash('Poster').body
+
+p OMDBGateway.gateway.free_search('Jedi').prune_array(1).body
+
